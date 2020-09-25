8ba7a4ef4a2a45ace58076c4fbd7dcc5c6af9991
Michael Adams <madams@gmail.com>
2020-09-24 19:33:25 -0600

    Actually do that thing

--- dotfiles/config/fish/functions/sudo.fish
+++ dotfiles/config/fish/functions/sudo.fish
@@ -2,8 +2,9 @@
 function sudo --description "Replacement for Bash 'sudo !!' command to run last command using sudo."
 	if test "$argv" = !!
 		#eval command sudo $history[1]
+		# This version will replace the history, so that's cool
 		commandline "sudo $history[1]"
-		history delete --prefix "sudo !!"
+		history delete --exact --case-sensitive 'sudo !!'
 		commandline -f execute
 	else
 		command sudo $argv
