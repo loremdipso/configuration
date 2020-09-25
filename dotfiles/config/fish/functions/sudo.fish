9dfc5dee9959f6163d3e58d0150d1ca28185f68a
Michael Adams <madams@gmail.com>
2020-09-24 19:25:38 -0600

    Aaaaand make sure we remove old 'sudo !!' from the history, too

--- dotfiles/config/fish/functions/sudo.fish
+++ dotfiles/config/fish/functions/sudo.fish
@@ -3,7 +3,7 @@ function sudo --description "Replacement for Bash 'sudo !!' command to run last 
 	if test "$argv" = !!
 		#eval command sudo $history[1]
 		commandline "sudo $history[1]"
-		# history delete --prefix "sudo !!"
+		history delete --prefix "sudo !!"
 		commandline -f execute
 	else
 		command sudo $argv
