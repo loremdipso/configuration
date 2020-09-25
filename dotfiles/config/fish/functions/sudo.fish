2b35b973c932d7626f8be485317352cc061ce086
Michael Adams <madams@gmail.com>
2020-09-24 19:24:22 -0600

    Updating sudo to work correctly in the history

--- dotfiles/config/fish/functions/sudo.fish
+++ dotfiles/config/fish/functions/sudo.fish
@@ -1,7 +1,10 @@
 
 function sudo --description "Replacement for Bash 'sudo !!' command to run last command using sudo."
 	if test "$argv" = !!
-		eval command sudo $history[1]
+		#eval command sudo $history[1]
+		commandline "sudo $history[1]"
+		# history delete --prefix "sudo !!"
+		commandline -f execute
 	else
 		command sudo $argv
 	end
